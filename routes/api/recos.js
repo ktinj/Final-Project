6b71b914313d89d55fbc1003e21a1fc13a65c899
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2021-01-14 12:43:00 -0600

    styling

--- routes/api/recos.js
+++ routes/api/recos.js
@@ -6,6 +6,10 @@ router.route("/")
   .get(recosController.findAll)
   .post(recosController.create);
 
+router
+  .route("/:keywords")
+  .get(recosController.findByKeywords)
+
 // Matches with "/api/comments/:id"
 // router
 //   .route("/:id")
