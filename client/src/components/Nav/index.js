d3130d7e710dfa7f6b5a534df7228d8f9e7b22d5
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2021-01-19 20:31:18 -0600

    added about modal

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NavLogo from "./PERCH (22).png";
+import AboutLogo from "./PERCH (23).png";
 import { Link } from "react-router-dom";
 import "./style.css";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
 
 function Nav () {
 
-    return (
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    return ( 
+        <>
         <nav>
             <ul className="navigation">
                 <Link to="/">
@@ -14,7 +25,22 @@ function Nav () {
                 <li className="nav-right">
                     <Link to={"/"}>Log out</Link></li>
                 <li className="nav-right">
-                    <Link to="./About">About</Link></li>
+                <Link onClick={handleShow}>About</Link></li> 
+        <Modal size="lg" show={show} onHide={handleClose} animation={false}>
+        <Modal.Header>
+        <Modal.Title><a><b>ABOUT</b></a></Modal.Title>
+        </Modal.Header>
+        <Modal.Body><p><img src={AboutLogo} height="35" alt="About Logo"/><br></br><br></br>
+        <b>Perch</b> is a one stop shop for unbiased, unpaid for, and uncensored <br></br>
+        recommendations from people you trust… <b>your friends!</b><br></br><br></br>
+        If you like something, put it on <b>Perch.</b><br></br></p>
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="light" onClick={handleClose}>
+        <a><b>Close</b></a>
+        </Button>
+        </Modal.Footer>
+        </Modal>
                 <li className="nav-right">
                     <Link to={"/uploadRec"}>Give a Recommendation</Link></li>
                 <li className="nav-right">
@@ -22,6 +48,7 @@ function Nav () {
                     
             </ul>
         </nav>
+        </>
     );
 
 }
