255a694ea15c2e6617ebfd4c3ca0db890dd449d5
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2021-01-20 20:10:20 -0600

    debug

--- client/src/pages/UploadRec.js
+++ client/src/pages/UploadRec.js
@@ -183,7 +183,7 @@ function UploadRec({ username }) {
                                         {uploadedFile ? (
                                             <div className='row mt-5'>
                                                 <div className='col-md-6 m-auto'>
-                                                    <p>className='text-center'>{uploadedFile.fileName}</p>
+                                                    <h3 className='text-center'>{uploadedFile.fileName}</h3> 
                                                     <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
                                                 </div>
                                             </div>
